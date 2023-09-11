1e41ea6c1034fae9f14d589747d3218716b739b8
GLVSKiriti <glvskiriti2003369@gmail.com>
2023-09-11 19:48:29 +0530

    Fixed folder structure
    Signed-off-by: GLVSKiriti <glvskiriti2003369@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Navbar from "./components/Navbar/Navbar";
+import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Navbar />
-      <Login />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from "react";
 import TeamVector from "../assets/teamvector";
 import MetaMask from "../components/Button/MetaMask";
-const Login = () => {
+import NavBar from "../components/Navbar/Navbar";
+const Landing = () => {
   // const [isConnected, setIsConnected] = useState(false);
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
@@ -58,34 +59,37 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="landing-page-container flex align-middle flex-col justify-center p-2 xl:p-20 bg-new-primary-bg-color items-center overflow-x-hidden gap-3.5rem h-screen pt-16">
-      <div className="landing-container flex-row-reverse  md:flex items-center h-50vh px-5">
-        <div className="landing-container">
-          {isMobileView && !isFoldView && (
-            <TeamVector width={328} height={262} />
-          )}
-          {isFoldView && <TeamVector width={250} height={242} />}
-          {!isMobileView && !isFoldView && (
-            <TeamVector width={488} height={358} />
-          )}
-        </div>
-        <div className="landing-page-text-container flex flex-col items-start gap-3 md:w-2/3">
-          <div className="landing-header-text mb-2 font-semibold text-3xl text-intro-text-color">
-            AcademiaGuard: <br /> Empowering the Future of Education
+    <>
+      <NavBar />
+      <div className="landing-page-container flex align-middle flex-col justify-center p-2 xl:p-20 bg-new-primary-bg-color items-center overflow-x-hidden gap-3.5rem h-screen pt-16">
+        <div className="landing-container flex-row-reverse  md:flex items-center h-50vh px-5">
+          <div className="landing-container">
+            {isMobileView && !isFoldView && (
+              <TeamVector width={328} height={262} />
+            )}
+            {isFoldView && <TeamVector width={250} height={242} />}
+            {!isMobileView && !isFoldView && (
+              <TeamVector width={488} height={358} />
+            )}
           </div>
-          <div className="landing-text mb-4 text-sm font-normal md:text-base text-body-text-color">
-            Welcome to AcademiaGuard, where the future of education meets
-            blockchain innovation. Our platform is your digital fortress,
-            securing academic records with the power of blockchain technology.
-            Say goodbye to worries of tampering or lost credentials. Join us on
-            a journey to redefine trust in education. Your success, digitally
-            protected.
+          <div className="landing-page-text-container flex flex-col items-start gap-3 md:w-2/3">
+            <div className="landing-header-text mb-2 font-semibold text-3xl text-intro-text-color">
+              AcademiaGuard: <br /> Empowering the Future of Education
+            </div>
+            <div className="landing-text mb-4 text-sm font-normal md:text-base text-body-text-color">
+              Welcome to AcademiaGuard, where the future of education meets
+              blockchain innovation. Our platform is your digital fortress,
+              securing academic records with the power of blockchain technology.
+              Say goodbye to worries of tampering or lost credentials. Join us
+              on a journey to redefine trust in education. Your success,
+              digitally protected.
+            </div>
+            <MetaMask />
           </div>
-          <MetaMask />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default Login;
+export default Landing;
