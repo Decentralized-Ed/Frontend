07e720c0e1ced28ac8e63e8d84ab02802f087d9b
saichidvi <saichidvigupta@gmail.com>
2023-09-28 09:54:39 +0530

    dashboard added

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/Navbar/Navbar";
+import DashBoardNavbar from "../components/DashboardNavbar/dashBoardNavbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <DashBoardNavbar />
+    </>
+  );
+};
+
+export default Dashboard;
