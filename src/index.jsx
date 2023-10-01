fb6698f8d277dffea34a4be69865902267d66f32
Joeyyy09 <imt_2021063@iiitm.ac.in>
2023-10-01 12:33:15 +0530

    Toast Update

--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { ToastProvider } from "react-toast-notifications";
 import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ToastProvider>
         <App />
-      </ToastProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
